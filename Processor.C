f21df2014f633971caafe51694badfe05771a4be
Paul Naidoo <p.naidoo.1@research.gla.ac.uk>
2021-02-26 14:25:47 +0000

    misc tweaks to input/output etc

--- Processor.C
+++ Processor.C
@@ -1,19 +1,51 @@
 
 {
+  // clas12databases::SetRCDBRootConnection("/w/work3/home/pauln/analysis/processed/rcdb/rcdb_allP1.root");
+  clas12databases::SetRCDBRootConnection("/w/work3/home/pauln/analysis/processed/rcdb/rcdb_skimmedP1.root");
+  // clas12databases::SetRCDBRootConnection("/w/work3/home/pauln/analysis/processed/rcdb/rcdb_tau_skimmedP1.root");
+
+  //clas12databases::SetRCDBRootConnection("/home/pauln/code/pi0analysis/rcdb_6336.root");
+
   
- clas12root::HipoChain chain;
- chain.SetReaderTags({0});
- //chain.Add("/work/jlab/clas12data/pass1_test/skim8_005051.hipo");
- chain.Add("/home/pauln/work/data/pass1v0/ndvcs_006*preskim.hipo");
- // chain.GetNRecords();
- chanser::HipoProcessor processor(&chain,"finalstates/FullAndPhotonCombs.txt");
- processor.AddOption("HIPOPROCESSOR_MAXPARTICLES","6");
+  clas12root::HipoChain chain;
+  chain.SetReaderTags({0});
+  //chain.Add("/w/work3/home/pauln/data/pass1v0/ndvcs_006*.hipo");
+  // chain.Add("/w/work3/home/pauln/data/skimmed_pass1v0/ndvcs_006*preskim.hipo");
 
- gBenchmark->Start("hd");
+  //Subset of Data:
+  // chain.Add("/w/work3/home/pauln/data/skimmed_pass1v0/ndvcs_0061*preskim.hipo"); //subset of ~80GB files
+  // chain.Add("/w/work3/home/pauln/data/skimmed_pass1v0/ndvcs_0062*preskim.hipo"); //--------- ^^
 
- gProof->Process(&processor,chain.GetNRecords());
- 
- gBenchmark->Stop("hd");
- gBenchmark->Print("hd");
+  //chain.Add("/w/work3/home/pauln/data/skimmed_pass1v0/ndvcs_0064*preskim.hipo"); //subset of 58 files chanser_MCsub
+  // chain.Add("/scratch/pauln/data/ndvcs_006*.hipo");
+  //chain.Add("/home/pauln/work/data/skimmed_pass1v0/ndvcs_006336_preskim.hipo");
+
+  // Batches for MC data:
+  //chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_14117.hipo");
+  //chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_1*.hipo"); //chanser_MCbig
+  // chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_11*.hipo"); //*
+  // chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_12*.hipo"); //*
+  // chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_13*.hipo"); //*
+  // chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_14*.hipo"); //*
+  chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_15*.hipo"); //*
+  
+  // chain.GetNRecords();
+  chanser::HipoProcessor processor(&chain,"finalstates/FullPhotonExclCombs.txt", "output/chanser_MC15");
+  //chanser::HipoProcessor processor(&chain,"finalstates/FullCombs_egTOPO.txt", "output/chanser_MC15_noneuttopo");
+  //chanser::HipoProcessor processor(&chain,"finalstates/FullPhotonExclCombs.txt", "output/chanser_MCbig");
+  //chanser::HipoProcessor processor(&chain,"finalstates/FullAndPhotonCombs.txt", "output/chanser_MCsub");
+  // chanser::HipoProcessor processor(&chain,"finalstates/FullPhotonExclCombs.txt", "output/chanser_ANAsub");
+
+  //chanser::HipoProcessor processor(&chain, "finalstates/FullAndPhotonCombs.txt", "output/chanser_PIDtest");
+
+  processor.AddOption("HIPOPROCESSOR_MAXPARTICLES","6");
+  //processor.AddOption("HIPOPROCESSOR_ANADB","$CHANSER/rga_actions/anadb/RGA_ACTIONS_PASS1.db");
+  processor.AddOption("HIPOPROCESSOR_ANADB","$CHANSER/anadbs/RunPeriodPass1.db"); //for MC target shift
+  gBenchmark->Start("hd");
+
+  gProof->Process(&processor,chain.GetNRecords());
+
+  gBenchmark->Stop("hd");
+  gBenchmark->Print("hd");
 
 }
--- Run_Pi0.C
+++ Run_Pi0.C
@@ -1,16 +1,28 @@
 {
   ////Set hipo file to be analysed
 
-  clas12databases::SetRCDBRootConnection("/home/pauln/code/pi0analysis/rcdb_6336.root");
+  //clas12databases::SetRCDBRootConnection("/home/pauln/code/pi0analysis/rcdb_6336.root");
+  clas12databases::SetRCDBRootConnection("/w/work3/home/pauln/analysis/processed/rcdb/rcdb_skimmedP1.root");
 
   HipoData hdata;
-  //hdata.SetFile("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_14117.hipo"); //pi0 MC sample file
-  hdata.SetFile("/home/pauln/work/data/pass1v0/ndvcs_006336.hipo");
+  //hdata.LoadAnaDB("$CHANSER/rga_actions/anadb/RGA_ACTIONS_PASS1.db");
+  hdata.LoadAnaDB("$CHANSER/anadbs/RunPeriodPass1.db");
+
+
+  //hdata.SetFile("/home/pauln/work/data/skimmed_pass1v0/ndvcs_006336_preskim.hipo");
+
+  hdata.SetFile("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_14117.hipo"); //pi0 MC sample file
+  //hdata.SetFile("/home/pauln/work/data/pass1v0/ndvcs_006336.hipo");    // data file
+  //hdata.SetFile("/w/work3/home/pauln/sim/OSG_dvcs/dst.hipo");          // OSG DVCS MC sample file
+  //hdata.SetFile("/w/work5/home/robertw/sims/osg_hipo_files/osg_0.hipo"); // OSG file from Robert
+
 
   ////create FinalStateManager
   ////we can load as many saved final states as we like
   FinalStateManager fsm;
-  fsm.SetBaseOutDir("output/6336_chanser.root");
+  //fsm.SetBaseOutDir("output/6336_chanser.root");
+  fsm.SetBaseOutDir("output/chanser_test/flag.root");
+
   ////Connect the data to the manager
   fsm.LoadData(&hdata);
 
