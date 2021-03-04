f7247b3d4157b240ff713d9e1b397d1f11103228
Paul Naidoo <p.naidoo.1@research.gla.ac.uk>
2021-03-04 09:47:39 +0000

    process larger subset of files

--- Processor.C
+++ Processor.C
@@ -23,15 +23,14 @@
   // Batches for MC data:
   //chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_14117.hipo");
   //chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_1*.hipo"); //chanser_MCbig
-  // chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_11*.hipo"); //*
-  // chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_12*.hipo"); //*
-  // chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_13*.hipo"); //*
-  // chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_14*.hipo"); //*
+  chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_11*.hipo"); //*
+  chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_12*.hipo"); //*
+  chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_13*.hipo"); //*
+  chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_14*.hipo"); //*
   chain.Add("/w/work3/home/pauln/sim/pi0/cooked_out_rgb_pi0_15*.hipo"); //*
   
   // chain.GetNRecords();
-  chanser::HipoProcessor processor(&chain,"finalstates/eff.txt", "output/chanser_MC15_eff");
-  //chanser::HipoProcessor processor(&chain,"finalstates/FullCombs_egTOPO.txt", "output/chanser_MC15_noneuttopo");
+  chanser::HipoProcessor processor(&chain,"finalstates/eff.txt", "output/chanser_MCsub_eff");
   //chanser::HipoProcessor processor(&chain,"finalstates/FullPhotonExclCombs.txt", "output/chanser_MCbig");
   //chanser::HipoProcessor processor(&chain,"finalstates/FullAndPhotonCombs.txt", "output/chanser_MCsub");
   // chanser::HipoProcessor processor(&chain,"finalstates/FullPhotonExclCombs.txt", "output/chanser_ANAsub");
@@ -41,6 +40,7 @@
   processor.AddOption("HIPOPROCESSOR_MAXPARTICLES","6");
   //processor.AddOption("HIPOPROCESSOR_ANADB","$CHANSER/rga_actions/anadb/RGA_ACTIONS_PASS1.db");
   processor.AddOption("HIPOPROCESSOR_ANADB","$CHANSER/anadbs/RunPeriodPass1.db"); //for MC target shift
+  processor.AddOption("HIPOPROCESSOR_RUNPERIOD","spring_2019");
   gBenchmark->Start("hd");
 
   gProof->Process(&processor,chain.GetNRecords());
